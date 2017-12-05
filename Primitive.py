78dd111d802ae5d03458ffa5f631b6ebcfd9ac1e
amata525 <kizuka525@gmail.com>
2017-12-05 17:55:28 +0900

    primitives

--- Primitive.py
+++ Primitive.py
@@ -22,25 +22,58 @@ def main():
     glutMainLoop()
 
 def init():
-    glClearColor(0.0, 0.2, 1.0, 1.0) # クリア色の指定
+    glClearColor(0.0, 0.0, 0.0, 1.0) # クリア色の指定
 
     # 座標系の設定
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluOrtho2D(-1.0, 1.0, -1.0, 1.0)
+    gluOrtho2D(0, 100, 0, 100)
 
 def display():
     """描画処理"""
     glClear(GL_COLOR_BUFFER_BIT) # 画面のクリア
 
     # 四角形を描く
-    glColor3f(1.0, 0.0, 0.0)
+    #glColor3f(1.0, 0.0, 0.0)
     glBegin(GL_QUADS)
-    glVertex2f(-0.5, -0.5)
-    glVertex2f(-0.5, 0.5)
-    glVertex2f(0.5, 0.5)
-    glVertex2f(0.5, -0.5)
+    glColor3f(1.0, 0.0, 0.0)  # 赤
+    glVertex2i(10, 10)
+    glColor3f(0.0, 1.0, 0.0)  # 緑
+    glVertex2i(20, 10)
+    glColor3f(1.0, 0.0, 0.0)  # 赤
+    glVertex2i(20, 20)
+    glColor3f(1.0, 1.0, 0.0)  # 黄色
+    glVertex2i(10, 20)
     glEnd()
+
+    # 点の描画
+    glColor3f(0.0, 0.2, 1.0)
+    glPointSize(5.0)
+    glBegin(GL_POINTS)
+    glVertex2i(30, 30)
+    glVertex2i(35, 35)
+    glVertex2i(40, 40)
+    glEnd()
+
+    # 線の描画
+    glColor3f(1.0, 1.0, 0.0)
+    glLineWidth(3.0)
+    glBegin(GL_LINES)
+    glVertex2f(31.4, 33.3)
+    glVertex2f(31.4, 55.5)
+    glEnd()
+
+    # ポリゴンの描画
+    glColor3f(0.7, 0.7, 0.7)
+    glBegin(GL_POLYGON)
+    glVertex2i(90, 90)
+    glVertex2i(95, 80)
+    glVertex2i(95, 70)
+    glVertex2i(90, 60)
+    glVertex2i(85, 70)
+    glVertex2i(85, 80)
+    glEnd()
+
 
     glFlush() # OpenGLコマンドの強制実行
 
